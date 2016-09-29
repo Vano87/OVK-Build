77debeec533dab2816bbf443a25feb16739da957
Vano87 <shabanovvano@gmail.com>
2016-09-29 15:15:18 +0300

    s-testmonials run

--- js/common.js
+++ js/common.js
@@ -1,27 +1,55 @@
 $(function() {
 
-	$(".carousel-eq, .carousel-brands").owlCarousel({
-			loop:true,
-    	responsive : {
-        0 : {
-            items:1,  
-        },
-        520 : {
-            items:1,
-        },
-        560 : {
-        		items:2,
-      	},
-        768 : {
-            items:2,
-        },
-        992 : {
-            items:3,
-        },
-        1200 : {
-            items:4,
-        }
-    }
+
+	$(".carousel-brands").owlCarousel({
+			loop : true,
+			margin : 30,
+			nav : true,
+			navText : ['<i class= "fa fa-angle-left"></i>','<i class= "fa fa-angle-right"></i>'],
+			responsive : {
+				0 : {
+						items:1,  
+				},
+				520 : {
+						items:1,
+				},
+				560 : {
+						items:2,
+				},
+				768 : {
+						items:2,
+				},
+				992 : {
+						items:3,
+				},
+				1200 : {
+						items:4,
+				}
+		}
+	});
+
+	$(".carousel-eq").owlCarousel({
+			loop : true,
+			responsive : {
+				0 : {
+						items:1,  
+				},
+				520 : {
+						items:1,
+				},
+				560 : {
+						items:2,
+				},
+				768 : {
+						items:2,
+				},
+				992 : {
+						items:3,
+				},
+				1200 : {
+						items:4,
+				}
+		}
 	});
 
 	$(".s-direct .item-vertical p").equalHeights();
@@ -56,7 +84,7 @@ $(function() {
 
 					 $.magnificPopup.proto.prev.call(self);
 
-					  }, 120);
+						}, 120);
 				}
 			},
 			imageLoadComplete: function() { 
